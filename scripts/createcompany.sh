683e48573f4a6e616d5fe11cb19784b8db8cde1e
Eysteinn Gunnlaugsson <eysteinn@Eysteinns-MacBook-Pro.local>
2015-11-10 13:50:58 +0000

    content type test

--- scripts/createcompany.sh
+++ scripts/createcompany.sh
@@ -7,4 +7,4 @@ curl -i -XPOST -d "{ \"title\" : \"Saffran\", \"description\" : \"rusl\", \"url\
 echo
 curl -i -XPOST -d "{ \"title\" : \"Wrong adminToken Company\", \"description\" : \"should fail\", \"url\" : \"http://www.fail.is\" }" -H "Content-Type: Application/json" -H "ADMIN_TOKEN: siggisaeti" http://localhost:3000/api/companies
 echo
-curl -i -XPOST -d "{ \"title\" : \"Saffran\", \"description\" : \"sama nafn og hitt saffran\", \"url\" : \"http://www.saffran.is\" }" -H "Content-Type: Application/json" -H "ADMIN_TOKEN: rssiprmp" http://localhost:3000/api/companies
+curl -i -XPOST -d "{ \"title\" : \"Annad bio\", \"description\" : \"flottara bio\", \"url\" : \"http://www.sambioin.is\" }" -H "Content-Type: Not/JSON" -H "ADMIN_TOKEN: rssiprmp" http://localhost:3000/api/companies
